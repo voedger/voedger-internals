9cf9e433a98d076898345b3e7b00240e23e3b8a9
michael-say <smmbox@gmail.com>
2025-11-26 15:54:02 +0100

    fix: update Docusaurus configuration for new site URL and base path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://voedger.github.io',
+  url: 'https://internals.voedger.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/voedger-internals/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
